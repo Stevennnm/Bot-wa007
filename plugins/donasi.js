57a5adf12dd7436b302cc3ddeba6dd9090249399
Stevennnm <78242243+Stevennnm@users.noreply.github.com>
2021-01-30 16:17:59 +0700

    Add files via upload

---
+++ plugins/donasi.js
@@ -0,0 +1,11 @@
+let handler = async m => m.reply(`
+╭─「 Donasi • Pulsa 」
+│ • Telkomsel [081264134328]
+│ • Telkomsel [082167459979]
+╰────
+`.trim()) // Tambah sendiri kalo mau
+handler.help = ['donasi']
+handler.tags = ['info']
+handler.command = /^dona(te|si)$/i
+
+module.exports = handler
